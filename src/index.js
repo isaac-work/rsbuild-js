ae2499625ef759683a2eac9737df268232e920b9
Isaac Huang <isaac.huang@westpharma.com>
2025-05-16 15:15:47 +0800

    u

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 
 document.querySelector('#root').innerHTML = `
 <div class="content">
-  <h1>Vanilla Rsbuild!!!</h1>
+  <h1>Vanilla Rsbuild!!!1234</h1>
   <img src="https://v2mobgfr806y00000.apps.k8s-stg.iotdev-westpharma.com/tracking-pixel" width="1" height="1" alt="" style="display:none;">
   <p>Start building amazing things with Rsbuild.</p>
 </div>
